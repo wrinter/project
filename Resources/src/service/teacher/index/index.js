afe06d11111bd8027063d47f9c724792ddb75dbe
chaoli129@163.com <12chao25tao>
2017-12-11 13:49:57 +0800

    自己的东西

--- Resources/src/service/teacher/index/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zxl on 2017/7/13.
- */
-export function getAd (context) {
-  return context.$http.post('/web/teacher/homepage/index/getTeacherSiteIndexAd');
-};
-export function getSiteIndex (context) {
-  return context.$http.post('/web/teacher/homepage/index/getSiteIndex');
-};
--- Resources/src/service/teacher/prepare.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zxl on 2017/7/20.
- */
-export function getCategory (context, param) {
-  return context.$http.post('/web/teacher/prepare/category', param, {'emulateJSON': true});
-};
